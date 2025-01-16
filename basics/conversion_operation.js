dde0f2d648ccb097495b076f93d6d49065b89199
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-16 17:43:50 +0000

    conversion operation

---
+++ basics/conversion_operation.js
@@ -0,0 +1,30 @@
+let score="33ab"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNuMBER=Number(score)
+// console.log(typeof valueInNuMBER);
+// console.log( valueInNuMBER);
+
+//"33"=>33
+//"33abs"=>Nan
+//true=>1 false=0
+//null=>0
+// undefined=>Nan
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 =>true 0=>false
+//""=>false
+//"ujjwal" =>true
+
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
