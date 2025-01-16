12816d219f5c51b0dbda72082d9b0e485f053363
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-16 17:17:51 +0000

    data type and variables

---
+++ basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //traet all js code as new version
+ //alert(3+3) // we are using nodejs,not browser
+
+ console.log(3+3)
+
+ let name="ujjwal"
+ let age =18
+ let isLoggedIn=false
+
+ // number =>2 to power 53
+ //bigint
+ //string=>""
+ //boolean=>true/false
+ // null=> standalone value
+ //undefined
+ //symbol=>unique
+
+
+ //object
+  console.log(typeof undefined)
+  console.log(typeof null)
--- basics/variables.js
+++ basics/variables.js
@@ -2,10 +2,12 @@ const accountId=14453
 let accountEmail="ujjwal@gmail.com"
 var accountPassword="12345"
 accountCity = "jaipur"
-
+let accountState;
 //accountId =2  not allowed
 
 accountEmail="jikk@nm.com"
 accountPassword="2665"
 accountCity="nainital"
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+//prefer not to use var keyword because opf block scope and functional scope
