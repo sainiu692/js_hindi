fbc6d0186336f283e5c9d81e79b29270a1de5a76
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-16 09:22:10 +0000

    new

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail="ujjwal@gmail.com"
+var accountPassword="12345"
+accountCity = "jaipur"
+
+//accountId =2  not allowed
+
+accountEmail="jikk@nm.com"
+accountPassword="2665"
+accountCity="nainital"
+console.table([accountId,accountEmail,accountPassword,accountCity])
