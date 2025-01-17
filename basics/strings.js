e8ad62f56e6e9bc5a5747b8487d3896e3654f480
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-17 17:01:15 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,7 @@
+const name="ujjwal"
+const repoCount=50
+
+// console.log(name+repoCount+" hello")
+// above is outdated syntax
+
+console.log(`hello my name is ${name} and my repo count is${repoCount}`);
