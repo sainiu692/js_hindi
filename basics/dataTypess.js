c490a2e9a362b7830250c46835f18a59437746ee
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-17 11:07:39 +0000

    new

---
+++ basics/dataTypess.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 types: string,boolean,number,null,undefined,symbol,bigInt;
+//they are pass by value
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+ let outsideTemp=null
+ let userEmail;
+
+ 
+
+ const id=Symbol('123')
+ const anotherId=Symbol('123')
+ console.log(id===anotherId)
+
+ //refrence or non-primitive
+ //array ,objects,functions
+ //they all have return type object and function has object function
+
+ //arrays
+ const heroes=["krish","doga"];
+
+ //objects
+ let myObj={
+    name:"ujjwal",
+    age:"22"
+ }
+
+ //function
+
+ const myFunction = function(){
+    console.log("hello world");
+ }
+
+//  https://tc39.es/ecma262/#sec-typeof-operator
+
