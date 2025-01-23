892cb01384f85e49b69f2839be4db7a39d060d62
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-23 18:49:30 +0000

    updation

---
+++ 02_basics/02_array.js
@@ -0,0 +1,31 @@
+const marvel_heros=["batman","spiderman"]
+const desi_heros=["shaktiman","naagraj"]
+
+// marvel_heros.push(desi_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[2][1]);
+
+// const allHeros=marvel_heros.concat(desi_heros)
+// console.log(allHeros)
+
+// const allNewHeros=[...marvel_heros, ...desi_heros]
+// console.log(allNewHeros);
+
+
+// const another_array =[1,2,3,[4,5,6,],7,[6,7,[4,2]]]
+// const anotherRealArray=another_array.flat(Infinity)
+// console.log(anotherRealArray)
+
+
+// console.log(Array.isArray("ujjwal"));
+// console.log(Array.from("ujjwal"));
+
+// console.log(Array.from({name:"ujjwal"}));
+//above case is interesting
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
