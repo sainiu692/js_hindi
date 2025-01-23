93925353b8a3f80e97d28904df58df18a09d317a
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-23 13:16:01 +0000

    math

---
+++ basics/nums_math.js
@@ -0,0 +1,28 @@
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(1000)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber=1123.899
+// console.log(otherNumber.toPrecision(5))
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++Maths+++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.23));
+// console.log(Math.ceil(4.23));
+// console.log(Math.floor(4.23));
+// console.log(Math.min(4,3,6,7,5));
+// console.log(Math.max(4,3,6,7,5));
+
+
+
+
