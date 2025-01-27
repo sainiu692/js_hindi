42ae0f63fd247c938b69b3829f2f218d2d1409f6
UJJWAL SAINI <116368406+sainiu692@users.noreply.github.com>
2025-01-27 16:52:44 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,15 +1,21 @@
+ //    Immediately Invoked Function Expressions (IIFE)
+//  It is commonly used to create a local scope and avoid polluting the global namespace.
+
+
+
 (function chai(){
     // named IIFE
-    console.log(`DB connected`);
-})();
+    console.log(`DB connected`); 
+})(); 
 // (fxn defn)()->this parenthesis calls the function 
 
 // (()=>{
     // unnamed IIFE 
 //     console.log(`DB connected two`);
-// })()
+// })();
 
 ((name)=>{
     // unnamed IIFE with parameter
     console.log(`DB connected to ${name}`);
-})('ujjwal')
+})('ujjwal');
+
